5867570ba8671cde6df95299e87ffd818e7c9808
mall <kevin.allg97@gmail.com>
2020-03-30 19:22:33 +0200

    Changed XOR's to OR's

--- Assets/Scripts/Timing/TrimAudio.cs
+++ Assets/Scripts/Timing/TrimAudio.cs
@@ -25,14 +25,13 @@ namespace NotReaper.Timing {
         public TrimAudio() {
             string ffmpegPath = Path.Combine(Application.streamingAssetsPath, "FFMPEG", "ffmpeg.exe");
 
-            if ((Application.platform == RuntimePlatform.LinuxEditor) ^ (Application.platform == RuntimePlatform.LinuxPlayer))
-                ffmpegPath = Path.Combine("/usr/bin/ffmpeg");
+            if ((Application.platform == RuntimePlatform.LinuxEditor) || (Application.platform == RuntimePlatform.LinuxPlayer))
+                ffmpegPath = Path.Combine(Application.streamingAssetsPath, "FFMPEG", "ffmpeg");
 
-            if ((Application.platform == RuntimePlatform.OSXEditor) ^ (Application.platform == RuntimePlatform.OSXPlayer))
-                ffmpegPath = Path.Combine("/usr/local/bin/ffmpeg");
+            if ((Application.platform == RuntimePlatform.OSXEditor) || (Application.platform == RuntimePlatform.OSXPlayer))
+                ffmpegPath = Path.Combine(Application.streamingAssetsPath, "FFMPEG", "ffmpegOSX");
 
-			ffmpeg.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
-			ffmpeg.StartInfo.FileName = ffmpegPath;
+            ffmpeg.StartInfo.FileName = ffmpegPath;
             ffmpeg.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
             ffmpeg.StartInfo.UseShellExecute = false;
             ffmpeg.StartInfo.RedirectStandardOutput = true;
